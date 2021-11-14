d49a16bdaa80a571b406586314b3363bde0749c6
Juan Campos <juancampos@Mac-Pro-de-Juan.local>
2021-11-14 11:44:47 -0300

    Tamplate add

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Welcome } from './src/components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome title={"Teste expo"}/>
       <StatusBar style="auto" />
     </View>
   );
