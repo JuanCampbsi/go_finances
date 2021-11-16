03937882e4d728e29af8cc4754518c9bc443104b
Juan Campos <juancampos.bsi@outlook.com>
2021-11-16 16:45:24 -0300

    Add dashboard

--- App.tsx
+++ App.tsx
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
+import { View } from 'react-native';
+import { Dashboard } from './src/screens/Dashboard';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcome title={"Teste expo"}/>
+    <View >
+      <Dashboard/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
