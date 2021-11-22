348ca97dc01a8d31fba1b0b8a9fa128d0d570902
Juan Campos <juancampos.bsi@outlook.com>
2021-11-22 15:02:22 -0300

    Add google font with expo-font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
+import  AppLoading  from 'expo-app-loading';
 
-import { Dashboard } from './src/screens/Dashboard';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
 import theme from './src/global/styles/theme'
 
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
+  const [fontsLoader] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+  
+  if(!fontsLoader){
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
