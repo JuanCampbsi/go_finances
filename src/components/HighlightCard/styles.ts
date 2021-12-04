b006be9b67fd13419b4402bd7a1b680c3c531621
Juan Campos <juancampos@iMac-de-Juan.local>
2021-12-03 22:13:55 -0300

    update highlightcard

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,10 +1,17 @@
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
 import {  RFValue } from 'react-native-responsive-fontsize';
+import theme from '../../global/styles/theme';
 
 
 export const Container = styled.View`
+  background-color: ${(theme.colors.shape)};
+  
+  width: ${RFValue(300)}px;
+  border-radius: 5px;
 
+  padding: 19px 23px;
+  padding-bottom: ${RFValue(42)}px;
 `;
 
 export const Header = styled.View `
@@ -20,6 +27,11 @@ export const Footer = styled.View `
 
 `;
 export const Amount = styled.Text `
+  font-family: ${(theme.fonts.medium)};
+
+  font-size: ${RFValue(32)}px;
+
+  color: ${(theme.colors.text_dark)};
 
 `;
 export const LastTransaction = styled.Text `
